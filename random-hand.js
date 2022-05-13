cd794ce428859dc5f188f8b194b039136b21508a
Michaël L <michaellaurac@gmail.com>
2022-05-13 18:37:19 +0200

    Generated a build card deck function and tested it

--- random-hand.js
+++ random-hand.js
@@ -1,6 +1,15 @@
 const suits = ['H', 'D', 'S', 'C'];
 const values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
+const buildCardArray = function () {
+  const tempArray = [];
+  for (let i = 0; i < values.length; i++) {
+    for (let j = 0; j < suits.length; j++) {
+      tempArray.push(values[i] + '-' + suits[j]);
+    }
+  }
+  return tempArray;
+};
 const randomSuit = function () {
   return suits[Math.floor(Math.random() * (suits.length))];
 };
@@ -23,6 +32,7 @@ const randomHand = function () {
 console.log(randomHand());
 
 module.exports = {
+  buildCardArray,
   randomHand,
   randomCard,
   randomValue,
--- test/random-hand-with-mocha.js
+++ test/random-hand-with-mocha.js
@@ -4,15 +4,35 @@
 
 // The following line allows recognition of mocha commands by ESLint
 /* eslint-env mocha */
-// const deepEqual = require('deep-equal');
+const deepEqual = require('deep-equal');
 const wish = require('wish');
 const {
+  buildCardArray,
   randomHand,
   randomCard,
   randomValue,
   randomSuit
 } = require('../random-hand');
 
+describe('buildCardArray()', function () {
+  it('gives a card array', function () {
+    wish(deepEqual(buildCardArray(), [
+      '1-H', '1-D', '1-S', '1-C',
+      '2-H', '2-D', '2-S', '2-C',
+      '3-H', '3-D', '3-S', '3-C',
+      '4-H', '4-D', '4-S', '4-C',
+      '5-H', '5-D', '5-S', '5-C',
+      '6-H', '6-D', '6-S', '6-C',
+      '7-H', '7-D', '7-S', '7-C',
+      '8-H', '8-D', '8-S', '8-C',
+      '9-H', '9-D', '9-S', '9-C',
+      '10-H', '10-D', '10-S', '10-C',
+      'J-H', 'J-D', 'J-S', 'J-C',
+      'Q-H', 'Q-D', 'Q-S', 'Q-C',
+      'K-H', 'K-D', 'K-S', 'K-C'
+    ]));
+  });
+});
 describe('randomHand()', function () {
   it('returns 5 random cards', function () {
     wish(randomHand().length === 5);
